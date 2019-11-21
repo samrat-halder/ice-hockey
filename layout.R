0765bbe0608c2268e729b85b9ed6e226da5a836a
Samrat Halder <samrathalder18@gmail.com>
2019-11-20 20:43:12 -0500

    Custom rink plot

---
+++ layout.R
@@ -0,0 +1,33 @@
+df <- structure(list(game_date = structure(c(17689, 17689, 17689, 17689, 
+                                             17689), class = "Date"), event_team = c("WSH", "WSH", "WSH", 
+                                                                                     "VGK", "VGK"), 
+                     event_description = c("WSH ONGOAL - #8 OVECHKIN, Deflected, Off. Zone, 10 ft. Expected Goal Prob: 25.7%", 
+                                           "WSH ONGOAL - #8 OVECHKIN, Wrist, Off. Zone, 38 ft. Expected Goal Prob: 4.9%", 
+                                           "WSH ONGOAL - #29 DJOOS, Wrist, Off. Zone, 65 ft. Expected Goal Prob: 1%", 
+                                           "VGK ONGOAL - #5 ENGELLAND, Wrist, Off. Zone, 38 ft. Expected Goal Prob: 1.5%", 
+                                           "VGK ONGOAL - #88 SCHMIDT, Wrist, Off. Zone, 62 ft. Expected Goal Prob: 1.3%"
+                     ), event_type = c(0, 0, 0, 0, 0), home_team = c("VGK", "VGK", "VGK", "VGK", "VGK"), away_team = c("WSH", "WSH", "WSH", "WSH", 
+                                         "WSH"), coords_x = c(-80, -53, -31, 56, 34), coords_y = c(1, 
+                                         -14, 30, -17, -26)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -5L)) 
+
+plotly_empty(df, x = ~coords_x, y=~coords_y, colors = c("black", "slategrey"))  %>% 
+  add_markers(size = ~event_type, alpha = 0.75, color = ~factor(event_team)) %>%
+  layout(
+    xaxis = list(range = c(-101, 101), title = ""), 
+    yaxis = list(range = c(-42.6, 42.6), title = ""),
+    images= list(
+      list(
+        source= "/Users/oblivion/Desktop/Courses/edav-final/Ice-Hockeyfull-rink.png",
+        xref= "x",
+        yref= "y",
+        x = 0,
+        y = 0,
+        sizex = 85.2,
+        sizey = 202,
+        opacity = 0.8,
+        layer = "below",
+        xanchor = "center",
+        yanchor = "middle"
+      )
+    )
+  )            
