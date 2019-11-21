86fdb40e45ffb411c4bf96e0c4b3ec17f96d7b54
samrat18 <samrathalder18@gmail.com>
2019-11-20 22:10:49 -0500

    Add layout on rink

--- layout.R
+++ layout.R
@@ -1,3 +1,4 @@
+library(plotly)
 df <- structure(list(game_date = structure(c(17689, 17689, 17689, 17689, 
                                              17689), class = "Date"), event_team = c("WSH", "WSH", "WSH", 
                                                                                      "VGK", "VGK"), 
@@ -10,24 +11,33 @@ df <- structure(list(game_date = structure(c(17689, 17689, 17689, 17689,
                                          "WSH"), coords_x = c(-80, -53, -31, 56, 34), coords_y = c(1, 
                                          -14, 30, -17, -26)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -5L)) 
 
-plotly_empty(df, x = ~coords_x, y=~coords_y, colors = c("black", "slategrey"))  %>% 
-  add_markers(size = ~event_type, alpha = 0.75, color = ~factor(event_team)) %>%
+
+image_file <- "full-rink.png"
+txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
+df %>% 
+  plot_ly(x = ~coords_x, y=~coords_y, 
+          text= ~event_description)  %>% 
+  add_markers(size = ~event_type,
+              sizes = c(150, 700),
+              
+              alpha = 0.75,
+              color = ~factor(event_team),
+              colors = c("black", "slategrey")
+  ) %>%
   layout(
-    xaxis = list(range = c(-101, 101), title = ""), 
-    yaxis = list(range = c(-42.6, 42.6), title = ""),
+    xaxis = list(autorange = TRUE), 
+    yaxis = list(autorange = TRUE),
     images= list(
-      list(
-        source= "/Users/oblivion/Desktop/Courses/edav-final/Ice-Hockeyfull-rink.png",
+        source= paste('data:image/png;base64', txt, sep=','),
         xref= "x",
         yref= "y",
         x = 0,
         y = 0,
-        sizex = 85.2,
-        sizey = 202,
+        sizex = 200,
+        sizey = 150,
         opacity = 0.8,
         layer = "below",
         xanchor = "center",
         yanchor = "middle"
-      )
-    )
-  )            
+        )
+  )
