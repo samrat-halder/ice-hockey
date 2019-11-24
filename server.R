df7b67834aeea490352d2ab3b6280b83a9b56814
harizjohnson <hariz_johnson@brown.edu>
2019-11-24 18:27:23 -0500

    realigned heatmap and filtered data
    added:
    - plotly rink will be the correct size upon running the app but not if you resize window
    - event types can be filtered using check boxes
    - replaced placeholder data with sample of cleaned data
    
    main filters dont do anything

--- server.R
+++ server.R
@@ -1,42 +1,58 @@
 server <-function(input, output) {
+  
+  load("~/GitHub/Ice-Hockey/data/2019-11-21_nhl-cleaned-data.RData")
   #Here goes the plots
   output$icemap <- renderPlotly({
     
+    
     library(plotly)
-    df <- structure(list(game_date = structure(c(17689, 17689, 17689, 17689, 
-                                                 17689), class = "Date"), event_team = c("WSH", "WSH", "WSH", 
-                                                                                         "VGK", "VGK"), 
-                         event_description = c("WSH ONGOAL - #8 OVECHKIN, Deflected, Off. Zone, 10 ft. Expected Goal Prob: 25.7%", 
-                                               "WSH ONGOAL - #8 OVECHKIN, Wrist, Off. Zone, 38 ft. Expected Goal Prob: 4.9%", 
-                                               "WSH ONGOAL - #29 DJOOS, Wrist, Off. Zone, 65 ft. Expected Goal Prob: 1%", 
-                                               "VGK ONGOAL - #5 ENGELLAND, Wrist, Off. Zone, 38 ft. Expected Goal Prob: 1.5%", 
-                                               "VGK ONGOAL - #88 SCHMIDT, Wrist, Off. Zone, 62 ft. Expected Goal Prob: 1.3%"
-                         ), event_type = c(0, 0, 0, 0, 0), home_team = c("VGK", "VGK", "VGK", "VGK", "VGK"), away_team = c("WSH", "WSH", "WSH", "WSH", 
-                                "WSH"), coords_x = c(-80, -53, -31, 56, 34), coords_y = c(1, -14, 30, -17, -26)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -5L)) 
+    #df <- structure(list(game_date = structure(c(17689, 17689, 17689, 17689, 
+    #                                             17689), class = "Date"), event_team = c("WSH", "WSH", "WSH", 
+    #                                                                                    "VGK", "VGK"), 
+    #                     event_description = c("WSH ONGOAL - #8 OVECHKIN, Deflected, Off. Zone, 10 ft. Expected Goal Prob: 25.7%", 
+    #                                           "WSH ONGOAL - #8 OVECHKIN, Wrist, Off. Zone, 38 ft. Expected Goal Prob: 4.9%", 
+    #                                           "WSH ONGOAL - #29 DJOOS, Wrist, Off. Zone, 65 ft. Expected Goal Prob: 1%", 
+    #                                           "VGK ONGOAL - #5 ENGELLAND, Wrist, Off. Zone, 38 ft. Expected Goal Prob: 1.5%", 
+    #                                           "VGK ONGOAL - #88 SCHMIDT, Wrist, Off. Zone, 62 ft. Expected Goal Prob: 1.3%"
+    #                     ), event_type = c(0, 0, 0, 0, 0), home_team = c("VGK", "VGK", "VGK", "VGK", "VGK"), away_team = c("WSH", "WSH", "WSH", "WSH", 
+    #                            "WSH"), coords_x = c(-80, -53, -31, 56, 34), coords_y = c(1, -14, 30, -17, -26)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -5L)) 
+    
+    
+    df <- vF_game_plays_2017[game.id == 2017020001 & (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL')]
+    if (input$shots == FALSE) {
+      df <- df[result.eventTypeId != 'SHOT']
+    }
+    if (input$goals == FALSE) {
+      df <- df[result.eventTypeId != 'GOAL']
+    }
+    if (input$hits == FALSE) {
+      df <- df[result.eventTypeId != 'HIT']
+    }
+  
     
     image_file <- "full-rink.png"
     txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
     df %>% 
-      plot_ly(x = ~coords_x, y=~coords_y, 
-              text= ~event_description)  %>% 
-      add_markers(size = ~event_type,
-                  sizes = c(150, 700),
+      plot_ly(x = ~coordinates.x, y=~coordinates.y)  %>% 
+      add_markers(
+        #size = ~event_type,
+                  size = 150,
                   
                   alpha = 0.75,
-                  color = ~factor(event_team),
-                  colors = c("black", "slategrey")
+                  color = ~factor(team.id.for),
+                  colors = c("dodgerblue", "darksalmon")
       ) %>%
       layout(
-        xaxis = list(autorange = TRUE), 
-        yaxis = list(autorange = TRUE),
+        xaxis = list(range = c(-120,120)),
+        yaxis = list(range = c(-60,60)),
         images= list(
           source= paste('data:image/png;base64', txt, sep=','),
           xref= "x",
           yref= "y",
           x = 0,
           y = 0,
-          sizex = 1000,
-          sizey = 500,
+          sizex = 260,
+          sizey = 160,
           opacity = 0.8,
           layer = "below",
           xanchor = "center",
--- ui.R
+++ ui.R
@@ -12,19 +12,27 @@ sidebar <- dashboardSidebar(
 )
 body <- dashboardBody(
   fluidRow(
-    box(solidHeader = T, collapsible = T, width = '80%',
-        title = "Inputs", status = "primary", background = "blue",
-      selectInput('homeTeam', 'Home Team', choices = c('Boston Bruins','New York Rangers'), selected = 'Boston Bruins', multiple = FALSE,
-                selectize = TRUE, width = NULL, size = NULL),
-      selectInput('awayTeam', 'Away Team', choices = c('Boston Bruins','New York Rangers'), selected = 'New York Rangers', multiple = FALSE,
-                  selectize = TRUE, width = NULL, size = NULL)
+    box(solidHeader = T, collapsible = T, width = '100%',
+        title = "Filters", status = "primary", background = "blue",
+        div(style="display: inline-block;vertical-align:top; width: 45%;",
+            titlePanel('Left'),
+            selectInput('leftTeam', 'Team', choices = c('Boston Bruins','New York Rangers'), selected = 'Boston Bruins', multiple = TRUE,
+                    selectize = TRUE, width = NULL, size = NULL),
+            selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                    selectize = TRUE, width = NULL, size = NULL)),
+        div(style="display: inline-block;vertical-align:top; width: 45%;",
+            titlePanel('Right'),
+            selectInput('leftTeam', 'Team', choices = c('Boston Bruins','New York Rangers'), selected = 'Boston Bruins', multiple = TRUE,
+                        selectize = TRUE, width = NULL, size = NULL),
+            selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                        selectize = TRUE, width = NULL, size = NULL))
     )
     
   ),
   fluidRow(
-    div(style="display: inline-block;vertical-align:top; width: 150px;",checkboxInput('shots', 'Shots', value = FALSE, width = NULL)),
-    div(style="display: inline-block;vertical-align:top; width: 150px;",checkboxInput('goals', 'Goals', value = FALSE, width = NULL)),
-    div(style="display: inline-block;vertical-align:top; width: 150px;",checkboxInput('hits', 'Hits', value = FALSE, width = NULL))
+    div(style="display: inline-block;vertical-align:top; width: 150px;",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+    div(style="display: inline-block;vertical-align:top; width: 150px;",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+    div(style="display: inline-block;vertical-align:top; width: 150px;",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
   ),
   fluidRow(
     #tags$img(src='full-rink-2.png', width = '100%'))
