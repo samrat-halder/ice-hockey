4871c3c57919371fb489502ab82e718e3a4c68a9
samrat18 <samrathalder18@gmail.com>
2019-11-25 14:59:14 -0500

    Add sidebar tabs for different seasons Currently there are lot of repeated codes, need to fix this later-- more elegant way

--- server.R
+++ server.R
@@ -1,15 +1,10 @@
 server <-function(input, output) {
-  
   #Here goes the plots
-  output$icemap <- renderPlotly({
-    
-    
-    #library(plotly)
-    
+  output$icemap_2017 <- renderPlotly(
+    {
     #get the ID of each team in the inputs
     left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
     right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
-    
     #filtering by teams and also by event types - probably a more elegant way to do this
     df <- vF_game_plays_2017[(team.id.for == left_team_id |  team.id.for == right_team_id) & (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL')]
     if (input$shots == FALSE) {
@@ -21,7 +16,6 @@ server <-function(input, output) {
     if (input$hits == FALSE) {
       df <- df[result.eventTypeId != 'HIT']
     }
-  
     #set the rink image and plot
     image_file <- "full-rink.png"
     txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
@@ -50,5 +44,98 @@ server <-function(input, output) {
           sizing = "stretch"
         )
       )
-  })
+    }
+  )
+  output$icemap_2016 <- renderPlotly(
+    {
+      #get the ID of each team in the inputs
+      left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
+      right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
+      #filtering by teams and also by event types - probably a more elegant way to do this
+      df <- vF_game_plays_2016[(team.id.for == left_team_id |  team.id.for == right_team_id) & (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL')]
+      if (input$shots == FALSE) {
+        df <- df[result.eventTypeId != 'SHOT']
+      }
+      if (input$goals == FALSE) {
+        df <- df[result.eventTypeId != 'GOAL']
+      }
+      if (input$hits == FALSE) {
+        df <- df[result.eventTypeId != 'HIT']
+      }
+      #set the rink image and plot
+      image_file <- "full-rink.png"
+      txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
+      df %>% 
+        plot_ly(x = ~coordinates.x, y=~coordinates.y, marker = list(size = 10))  %>% 
+        add_markers(
+          alpha = 0.25,
+          color = ~factor(team.id.for),
+          colors = c("dodgerblue", "darksalmon"),
+        ) %>%
+        layout(
+          xaxis = list(range = c(-110,110)),
+          yaxis = list(range = c(-50,50)),
+          images= list(
+            source= paste('data:image/png;base64', txt, sep=','),
+            xref= "x",
+            yref= "y",
+            x = 0,
+            y = 0,
+            sizex = 240,
+            sizey = 100,
+            opacity = 0.8,
+            layer = "below",
+            xanchor = "center",
+            yanchor = "middle",
+            sizing = "stretch"
+          )
+        )
+    }
+  )
+  output$icemap_2015 <- renderPlotly(
+    {
+      #get the ID of each team in the inputs
+      left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
+      right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
+      #filtering by teams and also by event types - probably a more elegant way to do this
+      df <- vF_game_plays_2015[(team.id.for == left_team_id |  team.id.for == right_team_id) & (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL')]
+      if (input$shots == FALSE) {
+        df <- df[result.eventTypeId != 'SHOT']
+      }
+      if (input$goals == FALSE) {
+        df <- df[result.eventTypeId != 'GOAL']
+      }
+      if (input$hits == FALSE) {
+        df <- df[result.eventTypeId != 'HIT']
+      }
+      #set the rink image and plot
+      image_file <- "full-rink.png"
+      txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
+      df %>% 
+        plot_ly(x = ~coordinates.x, y=~coordinates.y, marker = list(size = 10))  %>% 
+        add_markers(
+          alpha = 0.25,
+          color = ~factor(team.id.for),
+          colors = c("dodgerblue", "darksalmon"),
+        ) %>%
+        layout(
+          xaxis = list(range = c(-110,110)),
+          yaxis = list(range = c(-50,50)),
+          images= list(
+            source= paste('data:image/png;base64', txt, sep=','),
+            xref= "x",
+            yref= "y",
+            x = 0,
+            y = 0,
+            sizex = 240,
+            sizey = 100,
+            opacity = 0.8,
+            layer = "below",
+            xanchor = "center",
+            yanchor = "middle",
+            sizing = "stretch"
+          )
+        )
+    }
+  )
 }
--- ui.R
+++ ui.R
@@ -10,45 +10,126 @@ sidebar <- dashboardSidebar(
   #Here goes more customizations
   collapsed = TRUE,
   sidebarMenu(
+    menuItem('All Seasons',
+             menuSubItem('2017', tabName = '2017'),
+             menuSubItem('2016', tabName = '2016'),
+             menuSubItem('2015', tabName = '2015')
+    )
   )
 )
 body <- dashboardBody(
-  fluidPage(theme = shinytheme("slate"),
-    fluidRow(
-      align='center',
-      #collapsible box for main inputs
-      box(solidHeader = T, collapsible = T, width = '100%',
-          title = "Filters", status = "primary", background = "blue",
-          #input selections are inside div so we can place left and right inputs side by side
-          div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
-              titlePanel('Left'),
-              selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
-                      selectize = TRUE, width = NULL, size = NULL),
-              selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
-                      selectize = TRUE, width = NULL, size = NULL)),
-          div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
-              titlePanel('Right'),
-              selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
-                          selectize = TRUE, width = NULL, size = NULL),
-              selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
-                          selectize = TRUE, width = NULL, size = NULL))
+  tabItems(
+    tabItem('2017',
+        fluidPage(theme = shinytheme("slate"),
+          fluidRow(
+            align='center',
+            #collapsible box for main inputs
+            box(solidHeader = T, collapsible = T, width = '100%',
+                title = "Filters", status = "primary", background = "blue",
+                #input selections are inside div so we can place left and right inputs side by side
+                div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
+                    titlePanel('Left'),
+                    selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
+                            selectize = TRUE, width = NULL, size = NULL),
+                    selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                            selectize = TRUE, width = NULL, size = NULL)),
+                div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
+                    titlePanel('Right'),
+                    selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL),
+                    selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL))
+            )
+            
+          ),
+          #checkboxes for each event type (more to be added)
+          fluidRow(
+            align = "center",
+            div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+            div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+            div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+          ),
+          fluidRow(
+            align = "center", 
+            box(status = "primary", title = "Rink Layout", width = '100%',
+            plotlyOutput("icemap_2017"))
+          )
       )
-      
     ),
-    #checkboxes for each event type (more to be added)
-    fluidRow(
-      align = "center",
-      div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
-      div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
-      div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+    tabItem('2016',
+        fluidPage(theme = shinytheme("slate"),
+            fluidRow(
+              align='center',
+              #collapsible box for main inputs
+              box(solidHeader = T, collapsible = T, width = '100%',
+                title = "Filters", status = "primary", background = "blue",
+                #input selections are inside div so we can place left and right inputs side by side
+                div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
+                    titlePanel('Left'),
+                    selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL),
+                    selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL)),
+                div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
+                    titlePanel('Right'),
+                    selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL),
+                    selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL))
+            )
+              
+            ),
+            #checkboxes for each event type (more to be added)
+            fluidRow(
+              align = "center",
+              div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+              div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+              div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+            ),
+            fluidRow(
+              align = "center", 
+              box(status = "primary", title = "Rink Layout", width = '100%',
+                  plotlyOutput("icemap_2016"))
+            )
+        )
     ),
-    fluidRow(
-      align = "center", 
-      box(status = "primary", title = "Rink Layout", width = '100%',
-      plotlyOutput("icemap"))
+    tabItem('2015',
+        fluidPage(theme = shinytheme("slate"),
+          fluidRow(
+            align='center',
+            #collapsible box for main inputs
+            box(solidHeader = T, collapsible = T, width = '100%',
+              title = "Filters", status = "primary", background = "blue",
+              #input selections are inside div so we can place left and right inputs side by side
+              div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
+                  titlePanel('Left'),
+                  selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
+                              selectize = TRUE, width = NULL, size = NULL),
+                  selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                              selectize = TRUE, width = NULL, size = NULL)),
+              div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
+                  titlePanel('Right'),
+                  selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
+                              selectize = TRUE, width = NULL, size = NULL),
+                  selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                              selectize = TRUE, width = NULL, size = NULL))
+            )
+              
+            ),
+            #checkboxes for each event type (more to be added)
+            fluidRow(
+              align = "center",
+              div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+              div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+              div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+            ),
+            fluidRow(
+              align = "center", 
+              box(status = "primary", title = "Rink Layout", width = '100%',
+                  plotlyOutput("icemap_2015"))
+            )
+        )
     )
-    
   )
 )
-
 ui <- dashboardPage(header, sidebar, body, skin = "blue")
