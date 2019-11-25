42c7449e064ab995d3cbda199f81c2e967293188
samrat18 <samrathalder18@gmail.com>
2019-11-25 01:17:43 -0500

    UI changes

--- ui.R
+++ ui.R
@@ -13,39 +13,41 @@ sidebar <- dashboardSidebar(
   )
 )
 body <- dashboardBody(
-  fluidPage(
-  fluidRow(
-    align='center',
-    #collapsible box for main inputs
-    box(solidHeader = T, collapsible = T, width = '100%',
-        title = "Filters", status = "primary", background = "blue",
-        #input selections are inside div so we can place left and right inputs side by side
-        div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
-            titlePanel('Left'),
-            selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
-                    selectize = TRUE, width = NULL, size = NULL),
-            selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
-                    selectize = TRUE, width = NULL, size = NULL)),
-        div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
-            titlePanel('Right'),
-            selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
-                        selectize = TRUE, width = NULL, size = NULL),
-            selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
-                        selectize = TRUE, width = NULL, size = NULL))
+  fluidPage(theme = shinytheme("slate"),
+    fluidRow(
+      align='center',
+      #collapsible box for main inputs
+      box(solidHeader = T, collapsible = T, width = '100%',
+          title = "Filters", status = "primary", background = "blue",
+          #input selections are inside div so we can place left and right inputs side by side
+          div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
+              titlePanel('Left'),
+              selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
+                      selectize = TRUE, width = NULL, size = NULL),
+              selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                      selectize = TRUE, width = NULL, size = NULL)),
+          div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
+              titlePanel('Right'),
+              selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
+                          selectize = TRUE, width = NULL, size = NULL),
+              selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                          selectize = TRUE, width = NULL, size = NULL))
+      )
+      
+    ),
+    #checkboxes for each event type (more to be added)
+    fluidRow(
+      align = "center",
+      div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+      div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+      div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+    ),
+    fluidRow(
+      align = "center", 
+      box(status = "primary", title = "Rink Layout", width = '100%',
+      plotlyOutput("icemap"))
     )
     
-  ),
-  #checkboxes for each event type (more to be added)
-  fluidRow(
-    align = "center",
-    div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
-    div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
-    div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
-  ),
-  fluidRow(
-    align = "center",
-    plotlyOutput("icemap"), status = "warning", title = "", width = '100%', solidHeader = TRUE)
-  
   )
 )
 
