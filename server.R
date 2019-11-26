53b3ac38b0fbff868277c91011846b6ca80dfc78
samrat18 <samrathalder18@gmail.com>
2019-11-25 20:35:16 -0500

    Add tab for 2018

--- server.R
+++ server.R
@@ -1,5 +1,51 @@
 server <-function(input, output) {
   #Here goes the plots
+  output$icemap_2018 <- renderPlotly(
+    {
+      #get the ID of each team in the inputs
+      left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
+      right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
+      #filtering by teams and also by event types - probably a more elegant way to do this
+      df <- vF_game_plays_2018[(team.id.for == left_team_id |  team.id.for == right_team_id) & (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL')]
+      if (input$shots == FALSE) {
+        df <- df[result.eventTypeId != 'SHOT']
+      }
+      if (input$goals == FALSE) {
+        df <- df[result.eventTypeId != 'GOAL']
+      }
+      if (input$hits == FALSE) {
+        df <- df[result.eventTypeId != 'HIT']
+      }
+      #set the rink image and plot
+      image_file <- "full-rink.png"
+      txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
+      df %>% 
+        plot_ly(x = ~coordinates.x, y=~coordinates.y, marker = list(size = 10))  %>% 
+        add_markers(
+          alpha = 0.25,
+          color = ~factor(team.id.for),
+          colors = c("dodgerblue", "darksalmon"),
+        ) %>%
+        layout(
+          xaxis = list(range = c(-110,110)),
+          yaxis = list(range = c(-50,50)),
+          images= list(
+            source= paste('data:image/png;base64', txt, sep=','),
+            xref= "x",
+            yref= "y",
+            x = 0,
+            y = 0,
+            sizex = 240,
+            sizey = 100,
+            opacity = 0.8,
+            layer = "below",
+            xanchor = "center",
+            yanchor = "middle",
+            sizing = "stretch"
+          )
+        )
+    }
+  )
   output$icemap_2017 <- renderPlotly(
     {
     #get the ID of each team in the inputs
--- ui.R
+++ ui.R
@@ -11,6 +11,7 @@ sidebar <- dashboardSidebar(
   collapsed = TRUE,
   sidebarMenu(
     menuItem('All Seasons',
+             menuSubItem('2018', tabName = '2018'),
              menuSubItem('2017', tabName = '2017'),
              menuSubItem('2016', tabName = '2016'),
              menuSubItem('2015', tabName = '2015')
@@ -19,6 +20,42 @@ sidebar <- dashboardSidebar(
 )
 body <- dashboardBody(
   tabItems(
+    tabItem('2018',
+      fluidPage(theme = shinytheme("slate"),
+        fluidRow(
+          align='center',
+          #collapsible box for main inputs
+          box(solidHeader = T, collapsible = T, width = '100%',
+            title = "Filters", status = "primary", background = "blue",
+            #input selections are inside div so we can place left and right inputs side by side
+            div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
+                titlePanel('Left'),
+                selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
+                            selectize = TRUE, width = NULL, size = NULL),
+                selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                            selectize = TRUE, width = NULL, size = NULL)),
+            div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
+                titlePanel('Right'),
+                selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
+                            selectize = TRUE, width = NULL, size = NULL),
+                selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                            selectize = TRUE, width = NULL, size = NULL))
+          )
+        ),
+        #checkboxes for each event type (more to be added)
+        fluidRow(
+          align = "center",
+          div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+          div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+          div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+        ),
+        fluidRow(
+          align = "center", 
+          box(status = "primary", title = "Rink Layout", width = '100%',
+              plotlyOutput("icemap_2018"))
+        )
+      )
+    ),
     tabItem('2017',
         fluidPage(theme = shinytheme("slate"),
           fluidRow(
