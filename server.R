1a31fc53fc9f236b28774175e82f5b203688e9e9
samrat18 <samrathalder18@gmail.com>
2019-11-27 02:28:12 -0500

    Updated with latest 5 seasons data

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ server <-function(input, output) {
       left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
       right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
       #filtering by teams and also by event types - probably a more elegant way to do this
-      df <- vF_game_plays_2017_18[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
+      df <- vF_game_plays[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
                                     (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL') &
                                     (as.numeric(game.id) > 2017999999 & as.numeric(game.id) < 2019000000)]
       if (input$shots == FALSE) {
@@ -54,7 +54,7 @@ server <-function(input, output) {
     left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
     right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
     #filtering by teams and also by event types - probably a more elegant way to do this
-    df <- vF_game_plays_2017_18[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
+    df <- vF_game_plays[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
                                (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL') &
                                (as.numeric(game.id) > 2016999999 & as.numeric(game.id) < 2018000000)]
     if (input$shots == FALSE) {
@@ -102,7 +102,7 @@ server <-function(input, output) {
       left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
       right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
       #filtering by teams and also by event types - probably a more elegant way to do this
-      df <- vF_game_plays_2017_18[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
+      df <- vF_game_plays[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
                                     (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL') &
                                     (as.numeric(game.id) > 2015999999 & as.numeric(game.id) < 2017000000)]
       if (input$shots == FALSE) {
@@ -150,7 +150,7 @@ server <-function(input, output) {
       left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
       right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
       #filtering by teams and also by event types - probably a more elegant way to do this
-      df <- vF_game_plays_2017_18[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
+      df <- vF_game_plays[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
                                     (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL') &
                                     (as.numeric(game.id) > 2014999999 & as.numeric(game.id) < 2016000000)]
       if (input$shots == FALSE) {
@@ -191,5 +191,55 @@ server <-function(input, output) {
           )
         )
     }
+    
+  )
+  output$icemap_2014 <- renderPlotly(
+    {
+      #get the ID of each team in the inputs
+      left_team_id <- vF_teams_DT[long.name == input$leftTeam]$team.id
+      right_team_id <- vF_teams_DT[long.name == input$rightTeam]$team.id
+      #filtering by teams and also by event types - probably a more elegant way to do this
+      df <- vF_game_plays[(team.id.for == left_team_id |  team.id.for == right_team_id) & 
+                            (result.eventTypeId == 'SHOT' | result.eventTypeId == 'HIT' | result.eventTypeId == 'GOAL') &
+                            (as.numeric(game.id) > 2013999999 & as.numeric(game.id) < 2015000000)]
+      if (input$shots == FALSE) {
+        df <- df[result.eventTypeId != 'SHOT']
+      }
+      if (input$goals == FALSE) {
+        df <- df[result.eventTypeId != 'GOAL']
+      }
+      if (input$hits == FALSE) {
+        df <- df[result.eventTypeId != 'HIT']
+      }
+      #set the rink image and plot
+      image_file <- "full-rink.png"
+      txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
+      df %>% 
+        plot_ly(x = ~coordinates.x, y=~coordinates.y, marker = list(size = 10))  %>% 
+        add_markers(
+          alpha = 0.25,
+          color = ~factor(team.id.for),
+          colors = c("dodgerblue", "darksalmon"),
+        ) %>%
+        layout(
+          xaxis = list(range = c(-110,110)),
+          yaxis = list(range = c(-50,50)),
+          images= list(
+            source= paste('data:image/png;base64', txt, sep=','),
+            xref= "x",
+            yref= "y",
+            x = 0,
+            y = 0,
+            sizex = 240,
+            sizey = 100,
+            opacity = 0.8,
+            layer = "below",
+            xanchor = "center",
+            yanchor = "middle",
+            sizing = "stretch"
+          )
+        )
+    }
+    
   )
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 library(shinydashboard)
 library(plotly)
 library(shinythemes)
-load("./data/2019-11-25_nhl-cleaned-data.RData")
+load("./data/2019-11-26_nhl-cleaned-data.RData")
 
 header <- dashboardHeader(
   title = span("NHL ICE HOCKEY", 
@@ -16,19 +16,22 @@ sidebar <- dashboardSidebar(
              menuSubItem('2018', tabName = '2018_shots'),
              menuSubItem('2017', tabName = '2017_shots'),
              menuSubItem('2016', tabName = '2016_shots'),
-             menuSubItem('2015', tabName = '2015_shots')
+             menuSubItem('2015', tabName = '2015_shots'),
+             menuSubItem('2014', tabName = '2014_shots')
     ),
     menuItem('Season Statistics',
              menuSubItem('2018', tabName = '2018_stat'),
              menuSubItem('2017', tabName = '2017_stat'),
              menuSubItem('2016', tabName = '2016_stat'),
-             menuSubItem('2015', tabName = '2015_stat')
+             menuSubItem('2015', tabName = '2015_stat'),
+             menuSubItem('2014', tabName = '2015_stat')
     ),
     menuItem('Team Performance',
              menuSubItem('2018', tabName = '2018_perf'),
              menuSubItem('2017', tabName = '2017_perf'),
              menuSubItem('2016', tabName = '2016_perf'),
-             menuSubItem('2015', tabName = '2015_perf')
+             menuSubItem('2015', tabName = '2015_perf'),
+             menuSubItem('2014', tabName = '2014_perf')
     )
     
   )
@@ -184,6 +187,43 @@ body <- dashboardBody(
                   plotlyOutput("icemap_2015"))
             )
         )
+    ),
+    tabItem('2014_shots',
+            fluidPage(theme = shinytheme("slate"),
+                      fluidRow(
+                        align='center',
+                        #collapsible box for main inputs
+                        box(solidHeader = T, collapsible = T, width = '100%',
+                            title = "Filters", status = "primary", background = "blue",
+                            #input selections are inside div so we can place left and right inputs side by side
+                            div(style="display: inline-block;vertical-align:top; width: 45% ; margin-top: -1em;",
+                                titlePanel('Left'),
+                                selectInput('leftTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'Boston Bruins', multiple = FALSE,
+                                            selectize = TRUE, width = NULL, size = NULL),
+                                selectInput('leftHome', 'Home or Away', choices = c('Home','Away'), selected = 'Home', multiple = FALSE,
+                                            selectize = TRUE, width = NULL, size = NULL)),
+                            div(style="display: inline-block;vertical-align:top; width: 45%; margin-top: -1em;",
+                                titlePanel('Right'),
+                                selectInput('rightTeam', 'Team', choices = vF_teams_DT$long.name, selected = 'New York Rangers', multiple = FALSE,
+                                            selectize = TRUE, width = NULL, size = NULL),
+                                selectInput('rightHome', 'Home or Away', choices = c('Home','Away'), selected = 'Away', multiple = FALSE,
+                                            selectize = TRUE, width = NULL, size = NULL))
+                        )
+                        
+                      ),
+                      #checkboxes for each event type (more to be added)
+                      fluidRow(
+                        align = "center",
+                        div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('shots', 'Shots', value = TRUE, width = NULL)),
+                        div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('goals', 'Goals', value = TRUE, width = NULL)),
+                        div(style="display: inline-block;vertical-align:top; width: 150px; margin:-2em",checkboxInput('hits', 'Hits', value = TRUE, width = NULL))
+                      ),
+                      fluidRow(
+                        align = "center", 
+                        box(status = "primary", title = "Rink Layout", width = '100%',
+                            plotlyOutput("icemap_2014"))
+                      )
+            )
     )
   )
 )
