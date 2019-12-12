abe6665b34228b49753174e74254401f712b6db6
Jake Stamell <jakestamell@gmail.com>
2019-12-11 19:09:31 -0500

    Fixing warning

--- server.R
+++ server.R
@@ -426,7 +426,7 @@ server <-function(input, output, session) {
     dfGroupByPlayer <- aggregate(. ~ player.id, df, sum)
     dfGroupByPlayer <- merge(dfGroupByPlayer, vF_player_info[,c('player.id','firstName','lastName')], by='player.id')
     dfGroupByPlayer$Player <- paste(dfGroupByPlayer$firstName, dfGroupByPlayer$lastName, sep=' ')
-    dfGroupByPlayer <- subset(dfGroupByPlayer, select= names(dfGroupByPlayer) != c('player.id', 'firstName', 'lastName'))
+    dfGroupByPlayer <- subset(dfGroupByPlayer, select= !(names(dfGroupByPlayer) %in% c('player.id', 'firstName', 'lastName')))
     dfGroupByPlayer <- dfGroupByPlayer[,c('Player','stat.games','stat.goals','stat.shots','stat.assists')]
     colnames(dfGroupByPlayer) <- c('Name', 'Games', 'Goals', 'Shots', 'Assists')
     DT::datatable(dfGroupByPlayer, options = list(orderClasses = TRUE, pageLength = 5))
